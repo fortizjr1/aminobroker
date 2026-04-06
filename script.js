d6f36e757f36c52af1bbbf2c9c9213fbd6e9be80
Fernando Ortiz <fortizjr1@gmail.com>
2026-04-06 16:31:34 -0400

    Initial commit: Create sales website for AminoBroker

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const menuIcon = document.querySelector('.menu-icon');
+const navUl = document.querySelector('nav ul');
+
+menuIcon.addEventListener('click', () => {
+    navUl.classList.toggle('active');
+});
